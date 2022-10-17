3ce66371fcdfa4c8ed1f2a881c776a8a0faa4edd
lamson11 <lamson8204@gmail.com>
2022-10-17 22:55:30 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,70 @@
+from fileinput import filename
+from sre_constants import SUCCESS
+from flask import Flask, flash, request, redirect, url_for, render_template, Response
+from werkzeug.utils import secure_filename
+import os
+import cv2
+import urllib.request
+
+
+app = Flask(__name__)
+camera = cv2.VideoCapture(0)
+UPLOAD_FOLDER = 'static/uploads'
+app.secret_key = "cairocoders-ednalan"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def generate_frames():
+    while True:
+        ##read the camera frame
+        success, frame=camera.read()
+        if not success:
+            break
+        else:
+            ret,buffer=cv2.imencode('.jpg', frame)
+            frame=buffer.tobytes()
+        
+        yield(b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        flash('Image successfully uploaded and displayed below')
+        return render_template('index.html', filename=filename)
+    else:
+        flash('Allowed image type are - png, jpg, jpeg, gif')
+        return redirect(request.url)
+
+@app.route('/display/<filename>')
+def display_image(filename):
+    return redirect(url_for('static', filename='uploads/' + filename), code=301)
+
+@app.route('/webcamera')
+def webcamera():
+    return render_template('webcamera.html')
+
+@app.route('/webcamera/video')
+def video():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+    app.run(debug=True)
